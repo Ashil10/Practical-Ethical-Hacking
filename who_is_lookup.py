827a053fb72710b75b89785e501e053aaa3882d7
Ashil <ashilkp2003@gmail.com>
2023-12-30 19:04:53 +0530

    Add files via upload

---
+++ who_is_lookup.py
@@ -0,0 +1,15 @@
+import socket
+
+def whois_lookup(domain: str):
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect(("whois.iana.org", 43))
+    s.send(f"{domain}\r\n".encode())
+    response = s.recv(4096).decode()
+    s.close()
+    return response
+
+# Example usage:
+domain_name = "facebook.com"
+result = whois_lookup(domain_name)
+print(result)
+
